edbe6e68c469cb1ff3c692e9b57e94f87d99d237
sarah-altowaity1 <sa5793@nyu.edu>
2023-04-08 02:29:30 +0400

    change sound playing to play on user interaction

--- js/script.js
+++ js/script.js
@@ -12,9 +12,22 @@
 
 $(document).ready(function() {
     $('.scene-1').ripples({
-        resolution: 256,
-        dropRadius: 20,
-        perturbance: 0.03
+      resolution: 256,
+      dropRadius: 20,
+      perturbance: 0.03
     });
-    $('#sound').get(0).play();
-});
+  
+    $(document).ready(function() {
+        var sound = $('#sound').get(0);
+      
+        // Start playback when user interacts with the page
+        $(document).on("click touchstart", function() {
+          sound.play();
+        });
+      
+        // Rewind audio when it finishes playing
+        sound.addEventListener("ended", function() {
+          sound.currentTime = 0;
+        });
+      });
+  });
