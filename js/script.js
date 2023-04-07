133e63a8b65a98e67bcc2b65b8c7425f64a8c7e1
sarah-altowaity1 <sa5793@nyu.edu>
2023-04-07 05:03:42 +0400

    added start page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
+// $(document).ready(function() {
+//     $('.scene-1').ripples({
+//         resolution: 256,
+//         dropRadius: 20,
+//         perturbance: 0.03
+//     });
+// });
+
+// window.onload=function(){
+//     document.getElementById("sound").play();
+// }
+
 $(document).ready(function() {
     $('.scene-1').ripples({
         resolution: 256,
         dropRadius: 20,
         perturbance: 0.03
     });
+    $('#sound').get(0).play();
 });
