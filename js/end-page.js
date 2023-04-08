2b001ba31afdd4eb6740aaa1cf32043603a36bc7
LincolnAdomako <lna279@nyu.edu>
2023-04-08 18:19:00 +0400

    creating the end page

---
+++ js/end-page.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('.scene-1').ripples({
+      resolution: 256,
+      dropRadius: 20,
+      perturbance: 0.03
+    });
+  
+    var sound = $('#sound').get(0);
+    
+    // Start playback when user interacts with the page
+    $(document).on("click touchstart", function() {
+        sound.play();
+    });
+    
+    // Rewind audio when it finishes playing
+    sound.addEventListener("ended", function() {
+        sound.currentTime = 0;
+    });
+  });
--- js/scene-2.js
+++ js/scene-2.js
@@ -4,4 +4,16 @@ $(document).ready(function() {
       dropRadius: 20,
       perturbance: 0.07
     });
+
+    var sound = $('#sound').get(0);
+
+    // Start playback when user interacts with the page
+    $(document).on("click touchstart", function() {
+      sound.play();
+  });
+  
+  // Rewind audio when it finishes playing
+  sound.addEventListener("ended", function() {
+      sound.currentTime = 0;
+  });
   });
