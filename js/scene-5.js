876f6e1a6943d3e62694dd400e2553c1ca23a04e
sarah-altowaity1 <sa5793@nyu.edu>
2023-04-08 15:37:51 +0400

    added navigation buttons

---
+++ js/scene-5.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.scene-5').ripples({
+      resolution: 256,
+      dropRadius: 20,
+      perturbance: 0.07
+    });
+  });
--- js/script.js
+++ js/script.js
@@ -17,17 +17,15 @@ $(document).ready(function() {
       perturbance: 0.03
     });
   
-    $(document).ready(function() {
-        var sound = $('#sound').get(0);
-      
-        // Start playback when user interacts with the page
-        $(document).on("click touchstart", function() {
-          sound.play();
-        });
-      
-        // Rewind audio when it finishes playing
-        sound.addEventListener("ended", function() {
-          sound.currentTime = 0;
-        });
-      });
+    var sound = $('#sound').get(0);
+    
+    // Start playback when user interacts with the page
+    $(document).on("click touchstart", function() {
+        sound.play();
+    });
+    
+    // Rewind audio when it finishes playing
+    sound.addEventListener("ended", function() {
+        sound.currentTime = 0;
+    });
   });
