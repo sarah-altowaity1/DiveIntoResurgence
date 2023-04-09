0af438cae633461c9c614061dbb8ea936efb4ba7
LincolnAdomako <lna279@nyu.edu>
2023-04-09 17:05:25 +0400

    Animation for girl

--- js/scene-2.js
+++ js/scene-2.js
@@ -16,4 +16,29 @@ $(document).ready(function() {
   sound.addEventListener("ended", function() {
       sound.currentTime = 0;
   });
+
+
+  window.onload = function ()
+  {
+    var kid = document.getElementById("kid")
+    var rect = kid.getBoundingClientRect();
+    var newLeft = rect.left;
+    var newRight = rect.right;
+
+    kid.style.position = 'relative';
+
+    setInterval(function(){
+      newLeft = newLeft + 0.2;
+      newRight = newRight + 0.2;
+
+      if(newRight >= screen.width) {
+        newLeft = 0;
+        newRight = rect.width;
+      }
+
+      kid.style.left = newLeft + 'px';
+      kid.style.right = newRight + 'px';
+      
+    },1);
+  }
   });
